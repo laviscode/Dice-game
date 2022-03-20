db24c466088944f5c3a63c7660eb2b1736cc88d4
laviscode <91470572+laviscode@users.noreply.github.com>
2022-03-20 08:22:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-alert("You need to refresh the page and enter your name everytime you win or loose the game.")
-var playerName = prompt("Enter your name to play the game:");    
-var firstCharacterSlice = playerName.slice(0,1).toUpperCase();       //Capitalizing the first character in the input.
-var capitalizeWord= (firstCharacterSlice)+(playerName.slice(1));
-document.getElementById("name").innerHTML = capitalizeWord;         //Using capitalizeWord in HTML
+alert("🎲Welcome to the dice game.🎲\nThe higher number on the dice represents the winner")
+var player1 = "Player 1";      
+var player2 = "Player 2";
 
-var randomImage = new Array("images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png",)
-var randomImage1 = new Array("images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png",)
-
-var randomNumber = randomImage[Math.floor(Math.random()*(randomImage.length))];  //To generate  a random image from the array.
-var randomNumber1 = randomImage1[Math.floor(Math.random()*(randomImage1.length))];
-document.querySelectorAll("img")[0].setAttribute("src",randomNumber);             //Assigning random image to image tag in html 
-document.querySelectorAll("img")[1].setAttribute("src",randomNumber1);
-
-//Checking conditions to change the h1 attribute.
-if ( randomNumber > randomNumber1){    
-    document.querySelector("h1").innerHTML = "🚩"+capitalizeWord+" wins";
+//Function to change the player name!
+function editNames() {
+    player1 = prompt("Change player 1 name");
+    player2 = prompt("Change player 2 name");
+    document.querySelector("#name1").innerHTML = player1;
+    document.querySelector("#name2").innerHTML = player2;
 }
-else if ( randomNumber < randomNumber1){
-    document.querySelector("h1").innerHTML = "Player 2 wins🚩";
-}
-else{
-    document.querySelector("h1").innerHTML= "Oops, It's a draw."
+
+//Function to check the  conditions to change the h1 attribute.
+function rollFunction() {
+    var randomImage = new Array("images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png",)
+    var randomImage1 = new Array("images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png",)
+
+    var randomNumber = randomImage[Math.floor(Math.random() * (randomImage.length))];  //To generate  a random image from the array.
+    var randomNumber1 = randomImage1[Math.floor(Math.random() * (randomImage1.length))];
+    document.querySelectorAll("img")[0].setAttribute("src", randomNumber);             //Assigning random image to image tag in html 
+    document.querySelectorAll("img")[1].setAttribute("src", randomNumber1);
+    if (randomNumber > randomNumber1) {
+        document.querySelector("h1").innerHTML = "🥳🎉" + player1 + " wins";
+    }
+    else if (randomNumber < randomNumber1) {
+        document.querySelector("h1").innerHTML = player2 + " wins🥳🎊";
+    }
+    else {
+        document.querySelector("h1").innerHTML = "🤝It's a draw🤝"
+    }
 }
